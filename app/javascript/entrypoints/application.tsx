ace31a306aa24a3da4112a8a44442d97d0875c48
Naoki0722 <mattu.nao722@gmail.com>
2024-01-13 10:45:21 +0900

    install react

---
+++ app/javascript/entrypoints/application.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import App from '../App';
+
+function Main() {
+  return (<App />);
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Main />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
+  plugins: [RubyPlugin(),react()],
 })
